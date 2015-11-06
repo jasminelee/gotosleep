65b69200fe59517a6042781dc54670b6032ada0b
Jasmine <jasminelee@berkeley.edu>
2015-11-06 14:13:04 -0800

    added sound files

--- background.js
+++ background.js
@@ -1,7 +1,36 @@
-// create a HTML notification:
-var notification = webkitNotifications.createHTMLNotification(
-    'notification.html' // html url - can be relative
-);
+var now = new Date();
+var millisTill11 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0, 0, 0) - now;
+var halfHour = 1*60*1000 // minutes * seconds * milliseconds 
+var file_count = 0
+// if (millisTill10 < 0) {
+//      millisTill10 += 86400000; // it's after 10am, try 10am tomorrow.
+// }
 
-// Then show the notification.
-notification.show();
+//setTimeout only calls function once. setInterval calls function repeatedly 
+setTimeout(function(){
+  setInterval(function(){
+    createNotification();
+    audioNotification(file_count);
+  }, halfHour);
+}, millisTill11);
+
+
+
+function audioNotification(file_count){
+    var files = [];
+    for (i = 1; i <= 13; i++) {
+      files.push(new Audio('sleep'+i+'.m4a'));
+    }
+
+    var yourSound = files[file_count%14];
+    yourSound.play();
+    file_count += 1
+}
+
+function createNotification(){
+    var opt = {type: "basic", title: "Your Title", message: "Your message",iconUrl: "icon.png"}
+    chrome.notifications.create("notificationName",opt,function(){});
+
+    //include this line if you want to clear the notification after 5 seconds
+//    setTimeout(function(){chrome.notifications.clear("notificationName",function(){});},5000);
+}
