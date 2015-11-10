e6c18804beaf066b088b85935c7d3639eee2cca3
Jasmine <jasminelee@berkeley.edu>
2015-11-09 20:15:01 -0800

    added tool tip

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 var now = new Date();
-var millisTill11 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 14, 0, 0, 0) - now;
+var millisTill11 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 18, 0, 0, 0) - now;
 var halfHour = 1*30*1000 // minutes * seconds * milliseconds 
 
 var files = [];
@@ -11,16 +11,14 @@ function addFiles(){
 
 addFiles();
 
-// TODO: find char limit of text 
 // make notification last longer 
-// find bigger photo for notification icon 
 var text = [
 "Sleep Deprivation: delaying sleep after learning disrupts memory consolidation by affecting the neuronal signaling pathways in the hippocampus. (Prince, Abel. 2013).",
 "Memory Retention: sleep stabilizes declarative (fact-based) memory, offering immunity against forgetting. (Jenkins, Dalenbach. 1924.)", 
 "Sleep Deprivation: sleep deprived brains are, on average, smaller in volume and less populous in brain cells than well-rested brains. (Sexton, et. al. 2014).",
-"Alcohol and Memory: in a study examining memory and alcohol consumption, participants who drank alcohol only remembered 40-50% of information on a test after the first night, and only 60% after the the third night compared to the control group. (Sabia 2014). ", 
+"Alcohol and Memory: people who drank alcohol only remembered 40-50% of information on a test after the first night, and only 60% after the the third night. (Sabia 2014). ", 
 "Sleep Scheduling: students who had a regular bedtime performed better in language, reading, and math than students who went to bed at different times. (Gaylor 2010).", 
-"Emotional Brain Function: sleep prepares the brain for next-day social and emotional functioning. Accumulated sleep loss leads to an amplification of negative emotions. (Walker, 2009; Zohar, 2005). ", 
+"Emotion: sleep prepares the brain for next-day social and emotional functioning. Accumulated sleep loss leads to an amplification of negative emotions. (Walker, 2009). ", 
 "Naps and Recall: a short nap lasting 45 to 60 minutes produces a 5x improvement in information retrieval from memory. (Mecklinger 2015.)", 
 "Metabolism: reduced sleep quantity can impair regulation of appetite and metabolism, resulting in an increased risk of diabetes and obesity. (Tasali, 2007)",
 "Driving While Sleepy: after 22 hours of going without sleep, a person's driving is worse than someone who is legally drunk. (Williamson, Feyer, 2000.)",
@@ -34,7 +32,7 @@ var messages = [];
 
 function addSleepmessages(){
   for (i = 0; i <= 12; i++) {
-    var opt = {type: "basic", title: "Go to Sleep", message: text[i],iconUrl: "icon.png"}
+    var opt = {type: "basic", title: "Go to Sleep", message: text[i], priority:1, iconUrl: "book.png"}
     messages.push(opt);
   }
 }
@@ -51,7 +49,7 @@ function createNotification(file_count){
     chrome.notifications.create("notificationName",opt,function(){});
 
     //include this line if you want to clear the notification after 5 seconds
-    setTimeout(function(){chrome.notifications.clear("notificationName",function(){});},10000);
+    setTimeout(function(){chrome.notifications.clear("notificationName",function(){});},20*1000);
 }
 
 var file_count = 0
