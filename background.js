1d102a726df5bc00cf69b7e6c87b0e9fb1fdc14b
Jasmine <jasminelee@berkeley.edu>
2015-11-24 20:30:03 -0800

    GA in progress

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 var now = new Date();
-var millisTill11 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 19, 0, 0, 0) - now;
-var halfHour = 1*30*1000 // minutes * seconds * milliseconds 
+var millisTill11 = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 11, 0, 0, 0) - now;
+var halfHour = 15*60*1000 // minutes * seconds * milliseconds 
 
 var files = [];
 function addFiles(){
@@ -63,3 +63,16 @@ setTimeout(function(){
     file_count += 1
   }, halfHour);
 }, millisTill11);
+
+// Standard Google Universal Analytics code
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // Note: https protocol here
+ 
+ga('create', 'UA-41354756-5', 'auto');
+ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+ga('require', 'displayfeatures');
+ga('send', 'pageview', 'popup.html');
+
+
